7c81956611bcc346ec624d354a4e3974afc630a4
Jorge Neto <jfln@icomp.ufam.edu.br>
2021-05-08 23:11:11 -0300

    Segundo commit

---
+++ teste2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void imprime(){
+	printf("Jorge está aprendendo GIT\n");
+}
+
+void imprime2(){
+	printf("\n");
+}
+
+void imprime3(){
+	printf("Jorge continua aprendendo GIT\n");
+}
+
+int main(){
+	imprime();
+	imprime2();
+	imprime3();
+	
+	return 0;
+}
